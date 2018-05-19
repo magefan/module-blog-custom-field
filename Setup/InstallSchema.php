0fbb28cc2aafc62022383efdba2d68defae49f52
Ihor Vansach <ihor@magefan.com>
2018-05-19 10:20:49 +0300

    Initial commit

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ */
+
+namespace Magefan\BlogCustomField\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+
+/**
+ * Blog setup
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        /* Add one custom text field */
+        $table = $setup->getTable('magefan_blog_post');
+        $setup->getConnection()->addColumn(
+            $setup->getTable($table),
+            'custom_field',
+            [
+                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                'length' => 255,
+                'nullable' => true,
+                'comment' => 'Custom Field',
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}
